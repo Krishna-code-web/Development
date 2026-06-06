6432793ae1aef15fc1adbf39d4eb1f164d8aa1b9
Krishna-code-web <krishnabackup53@gmail.com>
2026-06-06 21:54:42 +0530

    Completed Module 1 of JavaScript Mastery

--- Phase 1 (JavaScript Mastery)/Module 1/Code/first.js
+++
@@ -1,6 +0,0 @@
-// console.log("Hello, World!");
-
-// let name = 'Krishna';
-// console.log("Hello, " + name + "!");
-
-// alert("Welcome to JavaScript!"); does not run on Node.js, it is for browsers
---
+++ Phase 1 (JavaScript Mastery)/Module 1/code/first.js
@@ -0,0 +1,17 @@
+// This module is all about the basics and theory of 
+// Javascript that's why there is not much code associated.
+
+// The most important thing in this module is 
+// how javascript code executes and how it engine 
+// runs the javascript code.
+
+// For just an example, using http server example.
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end("Hello World!");
+});
+
+server.listen(3000, () => {
+    console.log("Server is listening on http://localhost:3000");
+})
